c9ab669e06e7376949c5728df8ead8ef839bb84d
Alexey Skvortsov <alexeyn.skvortsov@gmail.com>
2020-07-04 02:54:26 +0700

    #12: remove unused ruuvi.scss

--- gzip_content.sh
+++ gzip_content.sh
@@ -2,5 +2,4 @@
 find . -type f -name '*.js' -exec ./yui_compress_gzip.sh "{}" \;
 find . -type f -name '*.html' -exec gzip -k -9 -f -v "{}" \;
 find . -type f -name '*.css' -exec ./yui_compress_gzip.sh "{}" \;
-find . -type f -name '*.scss' -exec ./yui_compress_gzip.sh "{}" css \;
 
