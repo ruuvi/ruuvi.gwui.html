83e35f81439614c260f25eee45999b75bac56119
Otso Jousimaa <otso@ojousima.net>
2020-07-06 17:24:57 +0300

    Fix config post (#16)
    Fix radio button loading, posting config

--- yui_compress_gzip.sh
+++ yui_compress_gzip.sh
@@ -1,6 +1,12 @@
 echo yui-compress and gzip: $1
+# Mac OSX brew installs as yuicompressor, try to detect which binary name is in system.
+COMPRESSOR="yui-compressor"
+linecnt=$( which $COMPRESSOR | wc -l)
+if [ $linecnt -eq 0 ] ; then
+  COMPRESSOR="yuicompressor"
+fi
 if [ "$2" != "" ]; then
-	yui-compressor --type $2 $1 | gzip -k -9 -f >$1.gz
+	$COMPRESSOR --type $2 $1 | gzip -k -9 -f >$1.gz
 else
-	yui-compressor $1 | gzip -k -9 -f >$1.gz
+	$COMPRESSOR $1 | gzip -k -9 -f >$1.gz
 fi
