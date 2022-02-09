83bf3e2f49e01e03ac05a1a057d656db19981982
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2022-02-09 20:06:36 +0700

    #171: scripts/gen_crypto_browserify_js.sh: run "npm install" if "node_modules" folder does not exist

--- scripts/gen_crypto_browserify_js.sh
+++ scripts/gen_crypto_browserify_js.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+set -x
 SCRIPT_PATH=$(dirname "$0")
-browserify -s crypto_browserify $SCRIPT_PATH/../crypto-browserify/index.js -o $SCRIPT_PATH/../src/crypto_browserify.js
+CRYPTO_BROWSERIFY=$SCRIPT_PATH/../crypto-browserify
+CRYPTO_BROWSERIFY_JS=$SCRIPT_PATH/../src/crypto_browserify.js
+
+if [ ! -d "$CRYPTO_BROWSERIFY/node_modules" ]; then
+  [ -f "$CRYPTO_BROWSERIFY_JS" ] && rm "$CRYPTO_BROWSERIFY_JS"
+  cd "$CRYPTO_BROWSERIFY" && npm install .
+  test $? -eq 0 || exit $?
+fi
+if [ ! -f "$CRYPTO_BROWSERIFY_JS" ]; then
+  browserify -s crypto_browserify "$CRYPTO_BROWSERIFY/index.js" -o "$CRYPTO_BROWSERIFY_JS"
+  test $? -eq 0 || exit $?
+fi
